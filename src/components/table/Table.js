8e16a29c2354df96bf65f95165cbdba68c420cfe
Vitalii Posypenko <posypenko.v@jilfond.ru>
2021-06-20 00:02:12 +0700

    refactor: перенос вёрстки в компоненты

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,25 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Table extends ExcelComponent {
+  static className = 'excel__table'
   toHTML() {
-    return '<h1>table </h1>';
+    return `
+       <div class="row">
+          <div class="row-info"></div>
+          <div class="row-data">
+              <div class="column">A</div>
+              <div class="column">B</div>
+              <div class="column">C</div>
+          </div>
+      </div>
+      <div class="row">
+          <div class="row-info">1</div>
+          <div class="row-data">
+              <div class="cell selected" contenteditable="true">a1</div>
+              <div class="cell" contenteditable="true">b1</div>
+              <div class="cell" contenteditable="true">c3</div>
+          </div>
+      </div>
+    `;
   }
-
 }
