dfa8f12446438d2b3cb2d545f68770a799ff85d0
Vitalii Posypenko <posypenko.v@jilfond.ru>
2021-09-05 13:10:33 +0700

    feat: связь поля ввода формул с ячейками таблицы

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,17 +4,34 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
     this.prepare();
   }
+  // Настратваем компонент до init
   prepare() {}
   // возвращает шаблон скомпонента
   toHTML() {
     return '';
   }
+  // уведомляем слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+  // подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
+  // инициализируем компонент
+  // добавляем dom слушателей
   init() {
     this.initDOMListeners();
   }
+  // удаляем компонент
+  // чистим слушателей
   destroy() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach(unsub => unsub());
   }
 }
