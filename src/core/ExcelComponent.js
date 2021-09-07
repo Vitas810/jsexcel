dfe220ae2910389c15945630f97cb11c5a033ed8
Vitalii Posypenko <posypenko.v@jilfond.ru>
2021-09-08 00:09:28 +0700

    создание store

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,8 +4,10 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.store = options.store;
     this.emitter = options.emitter;
     this.unsubscribers = [];
+    this.storeSub = null;
     this.prepare();
   }
   // Настратваем компонент до init
@@ -23,6 +25,12 @@ export class ExcelComponent extends DomListener {
     const unsub = this.emitter.subscribe(event, fn);
     this.unsubscribers.push(unsub);
   }
+  $dispatch(action) {
+    this.store.dispatch(action);
+  }
+  $subscribe(fn) {
+    this.storeSub = this.store.subscribe(fn);
+  }
   // инициализируем компонент
   // добавляем dom слушателей
   init() {
@@ -33,5 +41,6 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDOMListeners();
     this.unsubscribers.forEach(unsub => unsub());
+    this.storeSub.unsubscribe();
   }
 }
