ae332ee42a71f0aa4b6475c8b6e562cd0d0c2903
Vitalii Posypenko <posypenko.v@jilfond.ru>
2021-09-12 16:34:34 +0700

    feat: сохранение состояния таблицы в localStorage

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import {createStore} from './core/createStore';
 import {rootReducer} from './redux/rootReducer';
+import {storage} from './core/utils';
 import './scss/index.scss';
 
 const store = createStore(rootReducer, {
-  table: 'ss'
+  colState: {}
 });
 
+store.subscribe(state => {
+  storage('excel-state', state);
+});
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
   store
