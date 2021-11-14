875ab7ed6677f75d3720841fce1f5aa3df002bee
Vitalii Posypenko <posypenko.v@jilfond.ru>
2021-11-15 00:20:11 +0700

    сохранение стилей и названия таблицы

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,10 @@
-import {APPLY_STYLE, CHANGE_STYLES, CHANGE_TEXT, TABLE_RESIZE} from './types';
+import {
+  APPLY_STYLE,
+  CHANGE_STYLES,
+  CHANGE_TEXT,
+  CHANGE_TITLE,
+  TABLE_RESIZE,
+} from './types';
 // action creator
 export function tableResize(data) {
   return {
@@ -23,6 +29,13 @@ export function changeStyles(data) {
 export function applyStyle(data) {
   return {
     type: APPLY_STYLE,
+    data
+  };
+}
+
+export function changeTitle(data) {
+  return {
+    type: CHANGE_TITLE,
     data
   };
 }
